dd9ab79f1269c762ea3cef8afb48a46fdebea724
Edward Lavender <el72@st-andrews.ac.uk>
2023-05-27 13:36:34 +0200

    Add pf_dens() function

--- R/helper.R
+++ R/helper.R
@@ -77,6 +77,84 @@ ndp <- function(x){
   nchar(dp)
 }
 
+#### pf_dens()
+pf_dens <- function(xpf, coord = NULL, plot = TRUE,
+                    use_tryCatch = TRUE,
+                    ...) {
+
+  # Process raster
+  # spatstat assumes planar coordinates
+  raster::crs(xpf) <- NA
+
+  # Set up window
+  rim  <- maptools::as.im.RasterLayer(xpf)
+  rwin <- spatstat.geom::as.owin(rim)
+
+  # Get ppp
+  if (is.null(coord)) {
+    coord <- raster::rasterToPoints(xpf) |> as.data.frame()
+    colnames(coord) <- c("x", "y", "mark")
+    marks <- coord[, 3]
+  } else {
+    # Check coord
+    if (inherits(coord, "matrix")) {
+      coord <- as.data.frame(coord[, 1:2])
+      colnames(coord) <- c("x", "y")
+    }
+    if (!inherits(coord, "data.frame")) stop("`coord` should a data.frame.")
+    if (!all(colnames(coord) %in% c("x", "y"))) stop("`coord` should contain x and y columns.")
+    # Drop duplicate coordinates & adjust marks (weights) accordingly
+    # ... This assumes that coordinates that are uniquely define in `coord`
+    # ... are uniquely defined on xpf (see definition of weights below).
+    marks <- rep(1/nrow(coord), nrow(coord))
+    nrw_0 <- nrow(coord)
+    coord <-
+      coord |>
+      dplyr::mutate(mark = marks,
+                    key = paste(.data$x, .data$y)) |>
+      dplyr::group_by(.data$key) |>
+      dplyr::mutate(mark = sum(.data$mark)) |>
+      dplyr::slice(1L)
+    nrw_1 <- nrow(coord)
+    stopifnot(all.equal(1, sum(coord$mark)))
+    if (nrw_0 != nrw_1) warning("`coords` contains duplicate coordinates that have been processed.")
+    # Define marks/weights
+    marks <- coord$mark
+    rim <- raster::rasterize(x = coord[, c("x", "y")], y = xpf, field = coord$mark)
+    rim <- maptools::as.im.RasterLayer(rim)
+  }
+  rppp <- spatstat.geom::ppp(x = coord$x, y = coord$y, window = rwin, marks = marks)
+
+  # Get intensity (expected number of points PER UNIT AREA)
+  dens <- tryCatch(spatstat.explore::density.ppp(rppp, weights = rim, ...),
+                   error = function(e) e)
+  if (inherits(dens, "error")) {
+    if (!use_tryCatch) {
+      stop(dens)
+    } else {
+      warning(paste("\n", paste(dens, collapse = "\n ")), immediate. = TRUE, call. = FALSE)
+      return(NULL)
+    }
+  }
+  # Translate intensity into expected number of points PER PIXEL
+  dens <- raster::raster(dens)
+  dens <- dens * raster::area(dens)
+  # Translate expect counts into proportion of points per pixel
+  dens <- dens/raster::cellStats(dens, "sum")
+  stopifnot(all.equal(1, raster::cellStats(dens, "sum")))
+
+  # Plot
+  if (plot) {
+    ext <- raster::extent(xpf)
+    prettyGraphics::pretty_map(add_rasters = list(x = dens),
+                               xlim = ext[1:2], ylim = ext[3:4])
+  }
+
+  # Return map
+  dens
+
+}
+
 
 #### End of code.
 ######################################
